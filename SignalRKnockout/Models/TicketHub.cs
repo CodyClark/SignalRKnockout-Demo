699369164b3f5ac8f92edc48f1eb50d3936c8be5
Cody Clark <Cody31086@gmail.com>
2012-02-10 13:12:53 -0600

    Initial Import

---
+++ SignalRKnockout/Models/TicketHub.cs
@@ -0,0 +1,22 @@
+﻿
+using SignalR.Hubs;
+
+namespace SignalRKnockout.Models
+{
+    public class TicketHub : Hub
+    {
+        static int TotalTickets = 10;
+
+        public void GetTicketCount()
+        {
+            Clients.updateTicketCount(TotalTickets);
+        }
+
+        public void BuyTicket()
+        {
+            if (TotalTickets > 0)
+                TotalTickets -= 1;
+            Clients.updateTicketCount(TotalTickets);
+        }
+    }
+}
