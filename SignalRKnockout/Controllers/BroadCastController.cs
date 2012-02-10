adb7c38681a39029ba83067cced5bfb03bcf807f
Cody Clark <Cody31086@gmail.com>
2012-02-10 17:13:40 -0600

    Updates

---
+++ SignalRKnockout/Controllers/BroadCastController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SignalR.Hubs;
+using SignalRKnockout.Models;
+
+namespace SignalRKnockout.Controllers
+{
+    public class BroadCastController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            Hub hub = new MessageHub();
+            
+
+            return View();
+        }
+
+    }
+}
---
+++ SignalRKnockout/Models/MessageHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace SignalRKnockout.Models
+{
+    public class MessageHub : Hub
+    {
+        public static int MessageCount = 0;
+
+        public void GetMessageCount()
+        {
+            Clients.updateMessageCount(MessageCount);
+
+            for (var x = 0; x < 5; x++)
+            {
+                Clients.displayMessage("this is a test message:" + x.ToString());
+                MessageCount = MessageCount + 1;
+                Clients.updateMessageCount(MessageCount);
+            }
+        }
+
+        public void CheckForNewMessages()
+        {
+            Clients.displayMessage("this is a NEW message");
+
+            MessageCount = MessageCount + 1;
+            Clients.updateMessageCount(MessageCount);
+        }
+    }
+}
